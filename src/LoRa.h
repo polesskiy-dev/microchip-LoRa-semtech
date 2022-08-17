d707d5994f724c6336b8663a91c9060792523822
apolisskyi <apolisskyi@n-ix.com>
2022-08-18 01:56:01 +0300

    remove C++ code

--- src/LoRa.h
+++ src/LoRa.h
@@ -1,130 +1,95 @@
-// Copyright (c) Sandeep Mistry. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #ifndef LORA_H
 #define LORA_H
 
-#include <Arduino.h>
-#include <SPI.h>
-
-#if defined(ARDUINO_SAMD_MKRWAN1300)
-#define LORA_DEFAULT_SPI           SPI1
-#define LORA_DEFAULT_SPI_FREQUENCY 200000
-#define LORA_DEFAULT_SS_PIN        LORA_IRQ_DUMB
-#define LORA_DEFAULT_RESET_PIN     -1
-#define LORA_DEFAULT_DIO0_PIN      -1
-#elif defined(ARDUINO_SAMD_MKRWAN1310)
-#define LORA_DEFAULT_SPI           SPI1
-#define LORA_DEFAULT_SPI_FREQUENCY 200000
-#define LORA_DEFAULT_SS_PIN        LORA_IRQ_DUMB
-#define LORA_DEFAULT_RESET_PIN     -1
-#define LORA_DEFAULT_DIO0_PIN      LORA_IRQ
-#else
-#define LORA_DEFAULT_SPI           SPI
-#define LORA_DEFAULT_SPI_FREQUENCY 8E6 
-#define LORA_DEFAULT_SS_PIN        10
-#define LORA_DEFAULT_RESET_PIN     9
-#define LORA_DEFAULT_DIO0_PIN      2
+#ifdef	__cplusplus
+extern "C" {
 #endif
 
-#define PA_OUTPUT_RFO_PIN          0
-#define PA_OUTPUT_PA_BOOST_PIN     1
+#include <stdbool.h>
+#include <stdint.h>
 
-class LoRaClass : public Stream {
-public:
-  LoRaClass();
+uint32_t LoRaBegin(uint32_t frequency);
+void LoRaEnd();
 
-  int begin(long frequency);
-  void end();
+uint32_t LoRaBeginPacket(bool implicitHeader);
+uint32_t LoRaEndPacket(bool async);
 
-  int beginPacket(int implicitHeader = false);
-  int endPacket(bool async = false);
+uint32_t LoRaParsePacket(uint32_t size);
+uint32_t LoRaPacketRssi();
+float LoRaPacketSnr();
+uint32_t LoRaPacketFrequencyError();
 
-  int parsePacket(int size = 0);
-  int packetRssi();
-  float packetSnr();
-  long packetFrequencyError();
-
-  int rssi();
+uint32_t LoRaRssi();
 
   // from Print
-  virtual size_t write(uint8_t byte);
-  virtual size_t write(const uint8_t *buffer, size_t size);
+//  virtual size_t write(uint8_t byte);
+//  virtual size_t write(const uint8_t *buffer, size_t size);
 
   // from Stream
-  virtual int available();
-  virtual int read();
-  virtual int peek();
-  virtual void flush();
+//  virtual int available();
+//  virtual int read();
+//  virtual int peek();
+//  virtual void flush();
 
-#ifndef ARDUINO_SAMD_MKRWAN1300
-  void onReceive(void(*callback)(int));
-  void onTxDone(void(*callback)());
+void LoRaOnReceive(void(*callback)(uint32_t));
+void LoRaOnTxDone(void(*callback)(void ));
 
-  void receive(int size = 0);
+void LoRaReceive(uint32_t size);
+
+void LoRaIdle();
+void LoRaSleep();
+
+void LoRaSetTxPower(uint32_t level, uint32_t outputPin/* = PA_OUTPUT_PA_BOOST_PIN*/);
+void LoRaSetFrequency(uint32_t frequency);
+void LoRaSetSpreadingFactor(uint32_t sf);
+void LoRaSetSignalBandwidth(uint32_t sbw);
+void LoRaSetCodingRate4(uint32_t denominator);
+void LoRaSetPreambleLength(uint32_t length);
+void LoRaSetSyncWord(uint32_t sw);
+void LoRaEnableCrc();
+void LoRaDisableCrc();
+void LoRaEnableInvertIQ();
+void LoRaDisableInvertIQ();
+  
+void LoRaSetOCP(uint8_t mA); // Over Current Protection control
+  
+void LoRaSetGain(uint8_t gain); // Set LNA gain
+
+//void dumpRegisters(Stream& out);
+
+void LoRaExplicitHeaderMode();
+void LoRaImplicitHeaderMode();
+
+void LoRaHandleDio0Rise();
+bool LoRaIsTransmitting();
+
+uint32_t LoRaGetSpreadingFactor();
+uint32_t LoRaGetSignalBandwidth();
+
+void LoRaSetLdoFlag();
+
+uint8_t LoRaReadRegister(uint8_t address);
+void LoRaWriteRegister(uint8_t address, uint8_t value);
+uint8_t LoRaSingleTransfer(uint8_t address, uint8_t value);
+
+static void LoRaOnDio0Rise();
+
+//private:
+//  SPISettings _spiSettings;
+//  SPIClass* _spi;
+//  uint32_t _ss;
+//  uint32_t _reset;
+//  uint32_t _dio0;
+//  long _frequency;
+//  uint32_t _packetIndex;
+//  uint32_t _implicitHeaderMode;
+//  void (*_onReceive)(uint32_t);
+//  void (*_onTxDone)();
+
+#ifdef	__cplusplus
+}
 #endif
-  void idle();
-  void sleep();
-
-  void setTxPower(int level, int outputPin = PA_OUTPUT_PA_BOOST_PIN);
-  void setFrequency(long frequency);
-  void setSpreadingFactor(int sf);
-  void setSignalBandwidth(long sbw);
-  void setCodingRate4(int denominator);
-  void setPreambleLength(long length);
-  void setSyncWord(int sw);
-  void enableCrc();
-  void disableCrc();
-  void enableInvertIQ();
-  void disableInvertIQ();
-  
-  void setOCP(uint8_t mA); // Over Current Protection control
-  
-  void setGain(uint8_t gain); // Set LNA gain
-
-  // deprecated
-  void crc() { enableCrc(); }
-  void noCrc() { disableCrc(); }
-
-  byte random();
-
-  void setPins(int ss = LORA_DEFAULT_SS_PIN, int reset = LORA_DEFAULT_RESET_PIN, int dio0 = LORA_DEFAULT_DIO0_PIN);
-  void setSPI(SPIClass& spi);
-  void setSPIFrequency(uint32_t frequency);
-
-  void dumpRegisters(Stream& out);
-
-private:
-  void explicitHeaderMode();
-  void implicitHeaderMode();
-
-  void handleDio0Rise();
-  bool isTransmitting();
-
-  int getSpreadingFactor();
-  long getSignalBandwidth();
-
-  void setLdoFlag();
-
-  uint8_t readRegister(uint8_t address);
-  void writeRegister(uint8_t address, uint8_t value);
-  uint8_t singleTransfer(uint8_t address, uint8_t value);
-
-  static void onDio0Rise();
-
-private:
-  SPISettings _spiSettings;
-  SPIClass* _spi;
-  int _ss;
-  int _reset;
-  int _dio0;
-  long _frequency;
-  int _packetIndex;
-  int _implicitHeaderMode;
-  void (*_onReceive)(int);
-  void (*_onTxDone)();
-};
-
-extern LoRaClass LoRa;
 
 #endif
